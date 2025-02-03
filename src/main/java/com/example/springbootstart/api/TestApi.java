31305fb18747c67059651a69bb2619c7513a40c4
heeom <swimxdev@gmail.com>
2025-02-03 22:35:01 +0900

    feat : TestApi 클래스 추가

---
+++ src/main/java/com/example/springbootstart/api/TestApi.java
@@ -0,0 +1,27 @@
+package com.example.springbootstart.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/idempotency")
+public class TestApi {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestApi.class);
+
+    @PostMapping
+    public void post(@RequestBody String request) {
+        logger.info("post. request: {}", request);
+    }
+
+    @PutMapping
+    public void put(@RequestBody String request) {
+        logger.info("put. request: {}", request);
+    }
+
+    @GetMapping
+    public void get() {
+        logger.info("get");
+    }
+}
