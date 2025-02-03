3c6539914372580b1ffc0d7517fb0f22b51ba825
heeom <swimxdev@gmail.com>
2025-02-03 22:23:17 +0900

    feat : 요청 헤더의 idempotency-key를 redis에 저장하는 로직 구현

---
+++ src/main/java/com/example/springbootstart/interceptor/IdempotentRequestInterceptor.java
@@ -0,0 +1,43 @@
+package com.example.springbootstart.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.coyote.BadRequestException;
+import org.redisson.api.RBucket;
+import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+
+public class IdempotentRequestInterceptor implements HandlerInterceptor {
+
+    private static final String IDEMPOTENCY_KEY = "Idempotency-Key";
+
+    private static final Logger logger = LoggerFactory.getLogger(IdempotentRequestInterceptor.class);
+
+    private final RedissonClient redissonClient;
+
+    public IdempotentRequestInterceptor(RedissonClient redissonClient) {
+        this.redissonClient = redissonClient;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String key = request.getHeader(IDEMPOTENCY_KEY);
+        if (key == null) {
+            return true;
+        }
+
+        RBucket<String> bucket = redissonClient.getBucket(key);
+        boolean success = bucket.setIfAbsent(key, Duration.of(3, ChronoUnit.SECONDS));
+        if (!success) {
+            logger.error("duplicated request. key : {}", key);
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+}
