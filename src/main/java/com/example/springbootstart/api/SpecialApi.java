4fdec92bca7d92c72e3890b2c58a4256ca8e9d7b
swim <swimxdev@gmail.com>
2025-04-21 23:34:12 +0900

    feat : SpecialApi 구현

---
+++ src/main/java/com/example/springbootstart/api/SpecialApi.java
@@ -0,0 +1,20 @@
+package com.example.springbootstart.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/special")
+public class SpecialApi {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpecialApi.class);
+
+    @GetMapping
+    public String index() {
+        logger.info("SpecialApi index");
+        return "ok";
+    }
+}
