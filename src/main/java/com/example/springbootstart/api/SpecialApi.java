12355d26f790969c08d65a314b23d524ff0c628f
swim <swimxdev@gmail.com>
2025-04-21 23:36:27 +0900

    feat : SpecialController 어노테이션 적용

--- src/main/java/com/example/springbootstart/api/SpecialApi.java
+++ src/main/java/com/example/springbootstart/api/SpecialApi.java
@@ -1,5 +1,6 @@
 package com.example.springbootstart.api;
 
+import com.example.springbootstart.annotation.SpecialController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@SpecialController
 @RequestMapping("/api/special")
 public class SpecialApi {
 
--- src/main/java/com/example/springbootstart/exception/CommonExceptionHandler.java
+++ src/main/java/com/example/springbootstart/exception/CommonExceptionHandler.java
@@ -1,12 +1,13 @@
 package com.example.springbootstart.exception;
 
+import com.example.springbootstart.annotation.SpecialController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(annotations = SpecialController.class)
 public class CommonExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
