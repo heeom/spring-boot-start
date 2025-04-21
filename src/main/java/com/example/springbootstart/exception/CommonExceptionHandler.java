178bb5673ad2657e9e7179e8a056bd555e9d209e
swim <swimxdev@gmail.com>
2025-04-21 23:31:30 +0900

    rename : CommonExceptionHandler로 네이밍 변경

--- src/main/java/com/example/springbootstart/exception/CommonExceptionHandler.java
+++ src/main/java/com/example/springbootstart/exception/CommonExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class CustomExceptionHandler {
+public class CommonExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
